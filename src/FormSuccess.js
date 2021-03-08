a6f30a4213933967306cb9c709012e9713c67eb5
Sharmin <thelordcloud@gmail.com>
2021-03-08 10:02:51 +0200

    initial commit

---
+++ src/FormSuccess.js
@@ -0,0 +1,11 @@
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>We have received your request!</h1>
+    </div>
+  );
+};
+
+export default FormSuccess;
